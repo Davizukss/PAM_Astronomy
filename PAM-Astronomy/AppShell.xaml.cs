c80a416966ab66668b5bc2ac999f652ef7396fd8
davi <davivarelladev@gmail.com>
2024-09-18 11:09:40 -0300

    astronomy finish

---
+++ PAM-Astronomy/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PAM_Astronomy.Views;
+
+namespace PAM_Astronomy
+    
+    
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyView));
+        }
+    }
+}
